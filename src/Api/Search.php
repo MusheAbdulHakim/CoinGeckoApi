6203853147f963c9a3eaeee392f21a13e30b3cb9
musheabdulhakim <musheabdulhakim99@gmail.com>
2022-09-10 15:59:38 +0000

    initial commit

---
+++ src/Api/Search.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace MusheAbdulHakim\CoinGecko\Api;
+
+use MusheAbdulHakim\CoinGecko\Request;
+
+class Search extends Request {
+
+    public function query(string $query): array {
+        $params = [];
+        $params['query'] = $query;
+        return $this->get('/search',$params);
+    }
+}
