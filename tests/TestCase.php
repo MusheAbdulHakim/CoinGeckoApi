12b6d2c92f033e262419bf3d93c2554c43f349b3
musheabdulhakim <musheabdulhakim99@gmail.com>
2022-09-10 14:25:19 +0000

    initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+namespace MusheAbdulHakim\CoinGecko\Tests;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+class TestCase extends FrameworkTestCase{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
