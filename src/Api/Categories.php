cd869d3e5afd5d21c40b0349457883565b9c3b3b
musheabdulhakim <musheabdulhakim99@gmail.com>
2022-09-11 00:12:06 +0000

    initial commit

---
+++ src/Api/Categories.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace MusheAbdulHakim\CoinGecko\Api;
+
+use MusheAbdulHakim\CoinGecko\Request;
+
+
+class Categories extends Request {
+
+    /**
+     * List all categories
+     *
+     * @return array
+     */
+    public function getList(): array {
+       return $this->get('/coins/categories/list'); 
+    }
+
+    /**
+     * List all categories with market data
+     *
+     * @param string $order
+     * @return array
+     */
+    public function getListWithMarketData(string $order = 'market_cap_desc'): array {
+        $params['order'] = $order;
+        return $this->get('/coins/categories', $params);
+    }
+}
