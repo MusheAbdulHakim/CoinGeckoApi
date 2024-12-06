7e72704793c69308259c7c246055b710d85a6687
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-12-06 18:24:29 +0000

    exchangeRate,nft,search & trending added

--- src/Client.php
+++ src/Client.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 
 namespace MusheAbdulHakim\CoinGecko;
 
-use MusheAbdulHakim\CoinGecko\Api\AssetPlatforms;
-use MusheAbdulHakim\CoinGecko\Api\Categories;
+use MusheAbdulHakim\CoinGecko\Api\NFT;
 use MusheAbdulHakim\CoinGecko\Api\Ping;
 use MusheAbdulHakim\CoinGecko\Api\Coins;
+use MusheAbdulHakim\CoinGecko\Api\Search;
 use MusheAbdulHakim\CoinGecko\Api\Simple;
 use MusheAbdulHakim\CoinGecko\Api\Contract;
+use MusheAbdulHakim\CoinGecko\Api\Trending;
 use MusheAbdulHakim\CoinGecko\Api\Exchanges;
+use MusheAbdulHakim\CoinGecko\Api\Categories;
+use MusheAbdulHakim\CoinGecko\Api\Derivatives;
+use MusheAbdulHakim\CoinGecko\Api\ExchangeRate;
+use MusheAbdulHakim\CoinGecko\Api\AssetPlatforms;
 use MusheAbdulHakim\CoinGecko\Contracts\ClientContract;
 use MusheAbdulHakim\CoinGecko\Contracts\TransporterContract;
 
@@ -68,6 +73,31 @@ final readonly class Client implements ClientContract
     public function exchanges(): Exchanges
     {
         return new Exchanges($this->transporter);
+    }
+
+    public function derivates(): Derivatives
+    {
+        return new Derivatives($this->transporter);
+    }
+
+    public function nft(): NFT
+    {
+        return new NFT($this->transporter);
+    }
+
+    public function exchangeRate(): ExchangeRate
+    {
+        return new ExchangeRate($this->transporter);
+    }
+
+    public function search(): Search
+    {
+        return new Search($this->transporter);
+    }
+
+    public function trending(): Trending
+    {
+        return new Trending($this->transporter);
     }
 
 }
--- src/Contracts/ClientContract.php
+++ src/Contracts/ClientContract.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace MusheAbdulHakim\CoinGecko\Contracts;
 
+use MusheAbdulHakim\CoinGecko\Api\NFT;
 use MusheAbdulHakim\CoinGecko\Api\Ping;
 use MusheAbdulHakim\CoinGecko\Api\Coins;
+use MusheAbdulHakim\CoinGecko\Api\Search;
 use MusheAbdulHakim\CoinGecko\Api\Simple;
 use MusheAbdulHakim\CoinGecko\Api\Contract;
+use MusheAbdulHakim\CoinGecko\Api\Trending;
 use MusheAbdulHakim\CoinGecko\Api\Exchanges;
 use MusheAbdulHakim\CoinGecko\Api\Categories;
+use MusheAbdulHakim\CoinGecko\Api\Derivatives;
+use MusheAbdulHakim\CoinGecko\Api\ExchangeRate;
 use MusheAbdulHakim\CoinGecko\Api\AssetPlatforms;
 
 interface ClientContract
@@ -28,5 +33,17 @@ interface ClientContract
     public function categories(): Categories;
 
     public function exchanges(): Exchanges;
+
+    public function derivates(): Derivatives;
+
+    public function nft(): NFT;
+
+    public function exchangeRate(): ExchangeRate;
+
+    public function search(): Search;
+
+    public function trending(): Trending;
+
+
 
 }
